0e7e302f291f89877ca018a30dc5b05e0d781b65
arifasultana76 <arifa.rimi76@gmail.com>
2025-01-02 12:53:08 +0600

    assignment basic dart

---
+++ assignment_1.dart
@@ -0,0 +1,44 @@
+ans 1:
+
+void main() {
+  print('My name is Arifa.');
+  print('My favorite programming language is python.');
+  print(
+      'I am excited to learn dart because i would like to create an App and a website for myself.');
+}
+
+ans 2:
+
+void main() {
+  String name = "Arifa";
+  int age = 23;
+
+  print("Hello $name,");
+  print("You are $age years old!");
+  print("Welcome to Dart programming.");
+}
+
+ans 3:
+
+void main() {
+  int num1 = 30;
+  int num2 = 20;
+  int sum = num1 + num2;
+  int difference = num1 - num2;
+  int product = num1 * num2;
+
+  print(sum);
+  print(difference);
+  print(product);
+}
+
+ans 4:
+
+void main() {
+  double length = 10.5;
+  double width = 5.5;
+
+  double area = length * width;
+
+  print("The area of the rectangle is $area square units.");
+}
